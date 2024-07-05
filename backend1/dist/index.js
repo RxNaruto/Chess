7e016f194b19c2bb757da2d4807b7e76be2f1e08
Rithk <rithkchaudhary86@gmail.com>
2024-07-05 14:06:25 +0530

    backend done

--- backend1/dist/index.js
+++ backend1/dist/index.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
+const GameManager_1 = require("./GameManager");
 const wss = new ws_1.WebSocketServer({ port: 3000 });
+const gameManger = new GameManager_1.GameManager();
 wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
-    ws.on('message', function message(data) {
-        console.log('received data is %s', data);
-    });
-    ws.send('something');
+    gameManger.addUser(ws);
+    ws.on('disconnect', () => gameManger.removeUser(ws));
 });
